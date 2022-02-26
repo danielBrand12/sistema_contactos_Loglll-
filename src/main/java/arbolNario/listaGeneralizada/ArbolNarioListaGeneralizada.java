2c4a8a22de97754eefd449fccd25c305720d79e1
daniel <daniel@daniel-Aspire-A515-41G>
2022-02-26 11:48:20 -0500

    proyecto Lógica 3

---
+++ src/main/java/arbolNario/listaGeneralizada/ArbolNarioListaGeneralizada.java
@@ -0,0 +1,170 @@
+/*
+ * Copyright 2019 Carlos Alejandro Escobar Marulanda ealejandro101@gmail.com
+ * Permission is hereby granted, free of charge, to any person 
+ * obtaining a copy of this software and associated documentation 
+ * files (the "Software"), to deal in the Software without 
+ * restriction, including without limitation the rights to use, 
+ * copy, modify, merge, publish, distribute, sublicense, and/or 
+ * sell copies of the Software, and to permit persons to whom the 
+ * Software is furnished to do so, subject to the following 
+ * conditions:
+ * The above copyright notice and this permission notice shall 
+ * be included in all copies or substantial portions of the 
+ * Software.
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, 
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES 
+ * OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND 
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT 
+ * HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+ *  WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING 
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR 
+ * OTHER DEALINGS IN THE SOFTWARE.
+ */
+package arbolNario.listaGeneralizada;
+
+import sistema_contactos.BD_contactos.*;
+import sistema_contactos.contacto.Contacto;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Stack;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author Alejandro Escobar
+ */
+public class ArbolNarioListaGeneralizada {
+
+    NodoNario raiz = new NodoNario(null);
+    // Variables globales para identificar acciones
+    final static boolean TRANSFORMAR = true;
+    final static boolean NOTRANSFORMAR = false;
+
+    /**
+     * Otro constructor
+     *
+     * @param raiz
+     */
+    private ArbolNarioListaGeneralizada(NodoNario raiz) {
+        this.raiz = raiz;
+    }
+
+    public ArbolNarioListaGeneralizada() {
+    }
+    
+    public void insertarDatoNivel1(int celContacto){
+        NodoNario recorrido = raiz.getLiga();
+        NodoNario dato = new NodoNario(celContacto);
+        dato.setSw(0);
+        if()
+        //Se recorre el primer nivel del árbol Nario y se inserta el nuevo contacto al final de la lista
+        while(true){
+            if(recorrido.getLiga() == null){
+                recorrido.setLiga(dato);
+                break;
+            }
+            recorrido = recorrido.getLiga();
+        }
+    }
+    
+    
+    public void insertarDatoNivel2(int celContacto1, int celContacto2){
+        NodoNario recorrido = raiz.getLiga();
+        NodoNario dato = new NodoNario(celContacto2);
+        dato.setSw(0);
+        boolean encontrado = false;
+        
+        while(recorrido != null){
+            if(recorrido.getSw() == 0){
+                if((int)recorrido.getDato() == celContacto1){
+                    insertarDato(recorrido, celContacto2, TRANSFORMAR);
+                    encontrado = true;
+                    break;
+                }
+            }else if(recorrido.getSw() == 1){
+                //int aux =(int)((NodoNario)recorrido.getDato()).getDato();
+                if((int)((NodoNario)recorrido.getDato()).getDato() == celContacto1){
+                  insertarDato((NodoNario)recorrido.getDato(),celContacto2,NOTRANSFORMAR); 
+                  encontrado = true;
+                  break;
+                }
+            }
+            recorrido = recorrido.getLiga();
+        }
+        
+        if(!encontrado){
+            JOptionPane.showMessageDialog(null, "El contacto " + celContacto1 + " no se encuentra en la base de datos.");
+            //System.out.println("El contacto " + celContacto1 + " no se encuentra en la base de datos.");
+        }
+        
+    } 
+    
+    public void insertarDato(NodoNario subRaiz, int celContacto, boolean TRANSFORMAR){
+        NodoNario recorrido = subRaiz.getLiga();
+        NodoNario dato = new NodoNario(celContacto);
+        dato.setSw(0);
+        
+        //Caso en que la raiz no tiene contactos relacionados aún
+        if(TRANSFORMAR){
+            NodoNario nuevaRaiz = subRaiz;
+            subRaiz.setSw(1);
+            subRaiz.setLiga(nuevaRaiz);
+            nuevaRaiz.setLiga(dato);
+        }else{//Caso en el que la raíz ya cuenta con al menos un contacto 
+            while(true){
+            if(recorrido.getLiga() == null){
+                recorrido.setLiga(dato);
+                break;
+            }
+            recorrido = recorrido.getLiga();
+        }
+        }
+    }
+
+    /**
+     * Es una forma de pintar el arbol
+     */
+    void mostrarPorListas() {
+        Stack<NodoNario> migas = new Stack();
+        migas.add(raiz);
+        while (!migas.empty()) {
+            NodoNario pr = migas.pop();
+            while (pr != null) {
+                if (pr.getSw() == 0) {
+                    System.out.print(pr.getDato()); // Operar el recorrido
+                } else {
+                    NodoNario npr = (NodoNario) pr.getDato(); // Operar
+                    migas.add(npr);
+                }
+                pr = pr.getLiga();
+            }
+        }
+    }
+
+
+    /**
+     * Validar
+     */
+    public void imprimirXNivel() {
+        NodoNario r = raiz;
+        Queue<NodoNario> cola = new LinkedList<>();
+        while (r != null) {
+            if (r.getSw() == 1) {
+                NodoNario realHijo = (NodoNario) r.getDato();
+                System.out.print(realHijo.getDato()); // operación
+                cola.add(realHijo.getLiga());
+            } else {
+                System.out.print(r.getDato()); // operación
+            }
+            r = r.getLiga();
+            if (r == null && !cola.isEmpty()) {
+                r = cola.remove();
+            }
+        }
+    }
+
+    public NodoNario getRaiz() {
+        return raiz;
+    }
+
+}
